498d53e5c8726de1c7ad75828ae77c61966543a5
0kasama <okaajyad@gmail.com>
2024-05-29 21:14:31 +0700

    chore(review): add review component to product detail

--- src/fetch/review.js
+++ src/fetch/review.js
@@ -1,14 +1,60 @@
 import BASE_URL from "@/lib/baseUrl";
 import accessToken from "@/lib/token";
 
-export const getReviews = async () => {
-  const response = await fetch(`${BASE_URL}/reviews?id=1`, {
-    method: "GET",
+export const getAllReviews = async (id) => {
+  const res = await fetch(`${BASE_URL}/reviews?id=${id}`, {
     headers: {
-      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
     },
   });
+  const data = await res.json();
+  return data;
+}
 
-  const data = await response.json();
+
+export const getOneReview = async (id) => {
+  const res = await fetch(`${BASE_URL}/reviews/${id}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const data = await res.json();
+  return data;
+};
+
+export const createReview = async (body) => {
+  const res = await fetch(`${BASE_URL}/reviews`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+  return data;
+};
+
+export const updateReview = async (id, body) => {
+  const res = await fetch(`${BASE_URL}/reviews/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+  return data;
+};
+
+export const deleteReview = async (id) => {
+  const res = await fetch(`${BASE_URL}/reviews/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const data = await res.json();
   return data;
 };

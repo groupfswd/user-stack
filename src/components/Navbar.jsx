034be649f497e24508cedee674fe47ce997118b7
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-06-01 03:37:50 +0800

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,107 @@
 "use client";
 
 import Link from "next/link";
-import Person from "./person";
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+import { IoMdCart } from "react-icons/io";
+import { LiaSearchSolid } from "react-icons/lia";
 
 export default function Navbar() {
-  return (
-    <div className="bg-[#3797DB]">
-      <div className="flex justify-between container mx-auto text-white">
-        <Link href="/">
-          <h1 className="text-gray-800 font-bold text-3xl my-5">
-            Stack <span className="text-white">Masters</span>
-          </h1>
-        </Link>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [search, setSearch] = useState("");
 
-        <div className=" my-5 mx-10 gap-5 md:flex hidden">
-          <Link
-            className="text-2xl font-semibold hover:border-b-4 hover:text-blue-500"
-            href="/"
-          >
-            Home
-          </Link>
-          <Link
-            className="text-2xl font-semibold hover:border-b-4 hover:text-blue-600"
-            href="/wishlist"
-          >
-            Wishlist
-          </Link>
-          <Link
-            className="text-2xl font-semibold hover:border-b-4 hover:text-gray-100 hover:bg-blue-600 hover:rounded-lg"
-            href="/product"
-          >
-            Product
+  useEffect(() => {
+    if (Cookies.get("isLoggedIn") === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    Cookies.remove("accessToken");
+    Cookies.remove("isLoggedIn");
+    setIsLoggedIn(false);
+    window.location.reload();
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div className="navbar bg-[#3797db]">
+      <div className="flex-1">
+        <Link
+          href="/"
+          className="btn btn-ghost hover:bg-[#3187c5] text-white text-xl"
+        >
+          Baby Wonders
+        </Link>
+        <Link
+          href="/product"
+          className="btn btn-ghost hover:bg-[#3187c5] text-white text-xl"
+        >
+          Products
+        </Link>
+      </div>
+
+      <div className="flex-none gap-5">
+        <div className="form-control relative">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+            value={search}
+            onChange={handleSearch}
+          />
+          <Link href={`/product?search=${search}`}>
+            {" "}
+            <LiaSearchSolid className="text-3xl absolute top-2 right-2" />
           </Link>
         </div>
-        <div className="flex">
-          <Person />
+
+        <div className="text-2xl text-white">
+          <Link href="/cart">
+            <IoMdCart />
+          </Link>
+        </div>
+
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            {isLoggedIn ? (
+              <>
+                <li>
+                  <Link href="/user">Profile</Link>
+                </li>
+                <li>
+                  <Link href="/wishlist">Wishlist</Link>
+                </li>
+                <li>
+                  <a onClick={handleLogout} className="text-red-600">
+                    Logout
+                  </a>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+            )}
+          </ul>
         </div>
       </div>
     </div>
-  );
-}

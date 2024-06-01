d14c38b9a478dcd7b28c10b3364d9c1b86ec3c7a
amufid <91891129+amufid@users.noreply.github.com>
2024-06-02 06:29:55 +0700

    update placehoolder search price

--- src/components/SearchPrice.jsx
+++ src/components/SearchPrice.jsx
@@ -58,7 +58,7 @@ export default function SearchMinPrice() {
           value={searchMax}
           onChange={handleSearchMax}
           className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-          placeholder='Min Price...'
+          placeholder='Max Price...'
           required
         />
       </div>
